042332695e220b6d2d0e8f3299a0b9085a0ac64d
Kzorox <chrisrolin13@gmail.com>
2026-06-19 16:06:20 +0200

    working

--- tina/config.tsx
+++ tina/config.tsx
@@ -1,5 +1,4 @@
 import { defineConfig } from "tinacms";
-import nextConfig from '../next.config'
 
 import Post from "./collection/post";
 import Global from "./collection/global";
@@ -29,7 +28,7 @@ const config = defineConfig({
   build: {
     publicFolder: "public", // The public asset folder for your framework
     outputFolder: "admin", // within the public folder
-    basePath: nextConfig.basePath?.replace(/^\//, '') || '', // The base path of the app (could be /blog)
+    basePath: "", // Keep Tina decoupled from Next config unless a basePath is explicitly added here
   },
   schema: {
     collections: [Page, Post, Author, Tag, Global],
